be0652cc20fd89d69b56b6f8bf989d4763c96c59
Holderness <andersonholderness@gmail.com>
2015-06-17 13:00:20 -0400

    heroku db

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,9 @@
 var port = process.env.PORT || 1313;
+var db = process.env.MONGOLAB_URI || 'mongodb://localhost/library_database';
 
 module.exports = {
   port: port,
-  db: 'mongodb://localhost/library_database',
+  db: db,
   facebook: {
         clientID: '1683373935224797',
         clientSecret: '3675daf52fbe3271451c48030332714c',
